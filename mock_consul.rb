ca663cc55fff263bd1b87219d21b4df4b2d6a69c
Justin Bull <justin@wealthsimple.com>
2019-05-18 23:52:56 -0400

    Call consult's services catalog

--- mock_consul.rb
+++ mock_consul.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'securerandom'
-require 'concurrent'
 require 'pry-byebug'
 
 module SitRep
--- sitrep.rb
+++ sitrep.rb
@@ -1,11 +1,23 @@
 # frozen_string_literal: true
 
 require 'concurrent'
+require 'faraday'
 require 'pry-byebug' if ENV['RACK_ENV'] == 'development'
 
 module SitRep
   class Status
+    CONSUL_HOST = if ENV['RACK_ENV'] == 'development'
+                    ENV['CONSUL_HOST'] || '127.0.0.1:8500'
+                  else
+                    ENV['CONSUL_HOST'] or raise 'Set CONSUL_HOST'
+                  end.freeze
+
     def call(env)
+      response = Faraday.get "http://#{CONSUL_HOST}/v1/catalog/sexrvices"
+
+      return [500, { 'Content-Type' => 'application/json' }, ['TODO']] unless response.status == 200
+
+
       [200, {}, ['TODO']]
     end
   end
